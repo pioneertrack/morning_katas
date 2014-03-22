b4040cb8fd8616caaa8480882ae56f9be8565170
Steven Motley <stevenmotley89@gmail.com>
2014-03-22 10:50:22 -0400

    add instance of parser class to tests, add test to verify method sums hash values

--- remove_odd_hashes/parser.rb
+++ remove_odd_hashes/parser.rb
@@ -1,7 +1,25 @@
+require 'debugger'
+require 'ap'
+
 class Parser
 
-  def remove_odd_hashes(array, key_1, key_2)
-    #you can check for "%" operator
+  attr_reader :json
+
+  def initialize(json)
+    @json = json
+  end
+
+  def remove_odd_hashes
+    sum = 0
+    self.json.each do |hash|
+      hash.each do |key, value|
+        sum += value
+      end
+    end
+    0#you can check for "%" operator
   end
 
 end
+
+# myParser = Parser.new([{a: 5, b: 5}, {a: 3, b: 4}, {a: 2, b: 0}])
+# myParser.remove_odd_hashes
