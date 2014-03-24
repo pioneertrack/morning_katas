e89f4eec79f2f196502f212b75846a2341e50d76
Steven Motley <stevenmotley89@gmail.com>
2014-03-24 11:29:45 -0400

    refactor out redundant returns of myHash

--- ziiiiip/src/ziiip.js
+++ ziiiiip/src/ziiip.js
@@ -1,29 +1,23 @@
 function zipObject(params1, params2) {
   var myHash = {};
   var i = 0;
-  var j = 0;
-  if (params1 === undefined && params2 === undefined) {
+  if (!params1 && !params2) {
     return {};
   } else if (typeof (params1[0]) === 'object') {
     while (i < params1.length) {
       myHash[params1[i][0]] = params1[i][1];
       i++;
     }
-    return myHash;
   } else if (params1 && params2) {
-    while (i < params1.length && i < params2.length) {
+    while (i < params1.length) {
       myHash[params1[i]] = params2[i];
       i++;
     }
-    return myHash;
   } else if (params1 && params2 === undefined) {
     while (i < params1.length) {
       myHash[params1[i]] = undefined;
       i++;
     }
-    return myHash;
   }
+  return myHash;
 }
-
-
-
