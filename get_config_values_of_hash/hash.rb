1e9625efc68757f57c3121e58a96ec05970ffb10
Steven Motley <stevenmotley89@gmail.com>
2014-03-25 15:29:46 -0400

    satisfy all tests with hardcoded indices; must refactor to make method dynamic

--- get_config_values_of_hash/hash.rb
+++ get_config_values_of_hash/hash.rb
@@ -4,9 +4,13 @@ require 'ap'
 class Hash
 
   def get_value(default, *args) #'splat' or asterisk operator specifies an optional length variable list passed to the method as an array called args
-    args.each do |arg|
-      self[arg] || default
+    if args.length > 1
+      self[args[0]][args[1]] ||= default
+    else
+      self[args[0]] ||= default
     end
   end
 
 end
+
+
--- get_config_values_of_hash/spec.rb
+++ get_config_values_of_hash/spec.rb
@@ -13,7 +13,7 @@ describe Hash do
       end
 
       it 'should not raise an error if at least one parameters are given' do
-        expect{subject.get_value("")}.to_not raise_error(ArgumentError)
+        expect{subject.get_value("")}.not_to raise_error(ArgumentError)
       end
 
     end
@@ -21,19 +21,19 @@ describe Hash do
     context 'expected results' do
 
       it 'should return the value of a given key' do
-        expect(subject.get_value( "", :name)).to eq("config")
+        expect(subject.get_value("", :name)).to eq("config")
       end
 
       it 'should return the value of a given key' do
-        expect(subject.get_value( 0x0, :files, :mode )).to eq("0x777")
+        expect(subject.get_value(0x0, :files, :mode)).to eq(0x777)
       end
 
-      it 'should return the value of a given key' do
-        expect(subject.get_value( 80, :port )).to eq("80")
+      it 'should return the default value if a key does not exist' do
+        expect(subject.get_value(80, :port)).to eq(80)
       end
 
-      it 'should return the value of a given key' do
-        expect(subject.get_value( "cfg", :files, :extension )).to eq("cfg")
+      it 'should return the default value if a key does not exist' do
+        expect(subject.get_value("cfg", :files, :extension)).to eq("cfg")
       end
 
     end
@@ -41,22 +41,3 @@ describe Hash do
   end
 
 end
-
-
-# Description:
-
-# Write a simple method for the Hash-class that can get the configuration-value from a JSON-interpreted hash. Its signature is:
-
-# Hash.get_value( default, key1, ... )
-
-# key1, ... refer to the hash-keys of the JSON-interpreted hash, which is a hash of hash values. 
-
-#If the path of keys is found, it should return the found value, else it should return the default value.
-# Here are some examples on how to use it:
-
-# config = { :files => { :mode => 0x777 }, :name => "config" }
-
-# config.get_value( 0x0, :files, :mode ) == 0x777
-# config.get_value( "", :name ) == "config"
-# config.get_value( 80, :port ) == 80
-# config.get_value( "cfg", :files, :extension ) == "cfg"
