243931b9f5140f89a6aaf106bd18ab4e6f273a14
Steven Motley <stevenmotley89@gmail.com>
2014-03-25 11:39:11 -0400

    write rspec tests for context 'raise argument errors'

---
+++ get_config_values_of_hash/hash.rb
@@ -0,0 +1,7 @@
+class Hash
+
+  def get_value(default, *args)
+
+  end
+
+end
---
+++ get_config_values_of_hash/spec.rb
@@ -0,0 +1,40 @@
+require './hash'
+
+describe Hash do 
+
+  describe '#get_value' do
+
+    context 'parameter errors' do 
+
+      subject(:my_hash) { { :files => { :mode => 0x777 }, :name => "config" } }
+
+      it 'should raise an error if less than one parameter are given' do 
+        expect{subject.get_value}.to raise_error(ArgumentError) # note the curly brace syntax defines a block object upon which you can place the expectation; this allows rspec to catch the error and provide its own handling (cf. parantheses, which execute the code before passing it to rspec)
+      end
+
+      it 'should not raise an error if at least one parameters are given' do
+        expect{subject.get_value("")}.to_not raise_error(ArgumentError)
+      end
+    end
+
+  end
+
+end
+
+
+# Description:
+
+# Write a simple method for the Hash-class that can get the configuration-value from a JSON-interpreted hash. Its signature is:
+
+# Hash.get_value( default, key1, ... )
+
+# key1, ... refer to the hash-keys of the JSON-interpreted hash, which is a hash of hash values. If the path of keys is found, it should return the found value, else it should return the default value.
+
+# Here are some examples on how to use it:
+
+# config = { :files => { :mode => 0x777 }, :name => "config" }
+
+# config.get_value( 0x0, :files, :mode ) == 0x777
+# config.get_value( "", :name ) == "config"
+# config.get_value( 80, :port ) == 80
+# config.get_value( "cfg", :files, :extension ) == "cfg"
