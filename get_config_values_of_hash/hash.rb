691ad6982f5ba0171a42635e0dd7689c8b4e0e20
Steven Motley <stevenmotley89@gmail.com>
2014-03-25 12:08:26 -0400

    write rspec tests for context 'expected results'

--- get_config_values_of_hash/hash.rb
+++ get_config_values_of_hash/hash.rb
@@ -1,7 +1,12 @@
+require 'debugger'
+require 'ap'
+
 class Hash
 
-  def get_value(default, *args)
-
+  def get_value(default, *args) #'splat' or asterisk operator specifies an optional length variable list passed to the method as an array called args
+    args.each do |arg|
+      self[arg] || default
+    end
   end
 
 end
--- get_config_values_of_hash/spec.rb
+++ get_config_values_of_hash/spec.rb
@@ -4,9 +4,9 @@ describe Hash do
 
   describe '#get_value' do
 
-    context 'parameter errors' do 
+    subject(:my_hash) { { :files => { :mode => 0x777 }, :name => "config" } }
 
-      subject(:my_hash) { { :files => { :mode => 0x777 }, :name => "config" } }
+    context 'parameter errors' do 
 
       it 'should raise an error if less than one parameter are given' do 
         expect{subject.get_value}.to raise_error(ArgumentError) # note the curly brace syntax defines a block object upon which you can place the expectation; this allows rspec to catch the error and provide its own handling (cf. parantheses, which execute the code before passing it to rspec)
@@ -15,6 +15,27 @@ describe Hash do
       it 'should not raise an error if at least one parameters are given' do
         expect{subject.get_value("")}.to_not raise_error(ArgumentError)
       end
+
+    end
+
+    context 'expected results' do
+
+      it 'should return the value of a given key' do
+        expect(subject.get_value( "", :name)).to eq("config")
+      end
+
+      it 'should return the value of a given key' do
+        expect(subject.get_value( 0x0, :files, :mode )).to eq("0x777")
+      end
+
+      it 'should return the value of a given key' do
+        expect(subject.get_value( 80, :port )).to eq("80")
+      end
+
+      it 'should return the value of a given key' do
+        expect(subject.get_value( "cfg", :files, :extension )).to eq("cfg")
+      end
+
     end
 
   end
@@ -28,8 +49,9 @@ end
 
 # Hash.get_value( default, key1, ... )
 
-# key1, ... refer to the hash-keys of the JSON-interpreted hash, which is a hash of hash values. If the path of keys is found, it should return the found value, else it should return the default value.
+# key1, ... refer to the hash-keys of the JSON-interpreted hash, which is a hash of hash values. 
 
+#If the path of keys is found, it should return the found value, else it should return the default value.
 # Here are some examples on how to use it:
 
 # config = { :files => { :mode => 0x777 }, :name => "config" }
