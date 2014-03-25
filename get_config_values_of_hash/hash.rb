026b72495f2636ee29ea56852a8c6faccec75393
Steven Motley <stevenmotley89@gmail.com>
2014-03-25 19:12:24 -0400

    replace hard-coded values with 'first' and 'shift'

--- get_config_values_of_hash/hash.rb
+++ get_config_values_of_hash/hash.rb
@@ -5,13 +5,13 @@ class Hash
 
   def get_value(default, *keys) #'splat' or asterisk operator specifies an optional length variable list passed to the method as an array called keys
     if keys.length > 1 #if more than one arg
-      if self[keys[0]].class == Hash # and the first arg is a hash (we only test to see if the first arg is a hash, because a second argument can only follow if this is the case)
-        self[keys[0]][keys[1]] ||= default # return the value of the nested hash
+      if self[keys.first].class == Hash # and the first arg is a hash (we only test to see if the first arg is a hash, because a second argument can only follow if this is the case)
+        self[keys.shift][keys.first] ||= default # return the value of the nested hash
       else 
         default #otherwise return the default
       end
     else
-      self[keys[0]] ||= default #if only one key return its value or default
+      self[keys.first] ||= default #if only one key return its value or default
     end
   end
 
