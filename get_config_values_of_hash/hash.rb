89666508531e39a3086e993f817a6cca2c4026ec
Steven Motley <stevenmotley89@gmail.com>
2014-03-25 18:51:22 -0400

    clean up notes

--- get_config_values_of_hash/hash.rb
+++ get_config_values_of_hash/hash.rb
@@ -7,8 +7,8 @@ class Hash
     if keys.length > 1 #if more than one arg
       if self[keys[0]].class == Hash # and the first arg is a hash (we only test to see if the first arg is a hash, because a second argument can only follow if this is the case)
         self[keys[0]][keys[1]] ||= default # return the value of the nested hash
-      else # otherwise if keys[1] doesn't exist
-        default #return the default
+      else 
+        default #otherwise return the default
       end
     else
       self[keys[0]] ||= default # otherwise if only one key return its value or default
