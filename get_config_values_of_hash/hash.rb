640cf6342eb21e1defc9c20347d581d74f3c191e
Steven Motley <stevenmotley89@gmail.com>
2014-03-25 18:49:50 -0400

    refactor out superflous conditional because a second key is only possible if the first key is a hash; hence no need if the second key exists

--- get_config_values_of_hash/hash.rb
+++ get_config_values_of_hash/hash.rb
@@ -5,10 +5,8 @@ class Hash
 
   def get_value(default, *keys) #'splat' or asterisk operator specifies an optional length variable list passed to the method as an array called keys
     if keys.length > 1 #if more than one arg
-      if self[keys[0]].class == Hash # and the first arg is a hash key
+      if self[keys[0]].class == Hash # and the first arg is a hash (we only test to see if the first arg is a hash, because a second argument can only follow if this is the case)
         self[keys[0]][keys[1]] ||= default # return the value of the nested hash
-      elsif self[keys[1]] # but if first arg is not a hash key and a second arg exists, we know the second arg is not a possible key
-        self[keys[0]] ||= default # so return only the value of first key or default
       else # otherwise if keys[1] doesn't exist
         default #return the default
       end
