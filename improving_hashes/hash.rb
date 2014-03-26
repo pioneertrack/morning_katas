b0e05448ffc415731885f4459e30c1c2330c980b
Steven Motley <stevenmotley89@gmail.com>
2014-03-26 13:24:05 -0400

    refactor with ||=

--- improving_hashes/hash.rb
+++ improving_hashes/hash.rb
@@ -5,10 +5,8 @@ class Hash
   def method_missing(key, *arguments)
     if (self.methods-Object.methods).include?(key)
       self.key
-    elsif self.has_key?(key.to_s)
-      self[key.to_s]
     elsif self.has_key?(key)
-      self[key]
+      self[key.to_s] ||= self[key]
     else
       NoMethodError 
     end
