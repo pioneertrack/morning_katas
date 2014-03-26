aa70b196e0f45a3a85d5a06359767dfb2c13d47f
Steven Motley <stevenmotley89@gmail.com>
2014-03-26 13:01:53 -0400

    add negative tests with include matcher

--- remove_odd_hashes/spec.rb
+++ remove_odd_hashes/spec.rb
@@ -14,6 +14,16 @@ describe Parser do
       expect(myParser.remove_odd_hashes).to eq([{a: 10, b: 0}, {a: 20, b: 0}])
     end
 
+    it "should return only the hashes which keys sum to an even number" do
+      myParser = Parser.new([{a: 10, b: 0}, {a: 2, b: 1}, {a: 20, b: 0}, {a: 1, b: 6}])
+      expect(myParser.remove_odd_hashes).not_to include({a: 1, b: 6})
+    end
+
+    it "should return only the hashes which keys sum to an even number" do
+      myParser = Parser.new([{a: 10, b: 0}, {a: 2, b: 1}, {a: 20, b: 0}, {a: 1, b: 6}])
+      expect(myParser.remove_odd_hashes).not_to include({a: 2, b: 1})
+    end
+
   end
 
 end
