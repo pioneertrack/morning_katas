10e6d3250711b44a7099ae94baa417861e604cf4
Steven Motley <stevenmotley89@gmail.com>
2014-03-27 12:18:17 -0400

    add palindrom kata, write jasmine tests, and define function palindrome

---
+++ palindrome/src/palindrome.js
@@ -0,0 +1,35 @@
+function palindrome () {
+  
+}
+
+
+
+
+
+
+
+// Test.assert_equals(palindrome("Amore, Roma"), true)
+// Test.assert_equals(palindrome("A man, a plan, a canal: Panama"), true)
+// Test.assert_equals(palindrome("No 'x' in 'Nixon'"), true)
+// Test.assert_equals(palindrome("Abba Zabba, you're my only friend"), false)
+
+// function zipObject(keys, values) {
+//   var myHash = {};
+//   var i = 0;
+//   if (!keys && !values) {
+//     return {};
+//   } else {
+//     while (i < keys.length) {
+//       if (Array.isArray(keys[0])) {
+//         myHash[keys[i][0]] = keys[i][1];
+//       } else if (keys && values) {
+//         myHash[keys[i]] = values[i];
+//       } else if (keys && values === undefined) {
+//         myHash[keys[i]] = undefined;
+//       }
+//       i++;
+//     }
+//     return myHash;
+//   }
+// }
+
