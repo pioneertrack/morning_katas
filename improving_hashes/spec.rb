442d665c2c7a99d5aafc69d0bd4c45670bbf8cce
Steven Motley <stevenmotley89@gmail.com>
2014-03-28 09:00:04 -0400

    create slope files, write jasmine tests for #getSlope function

--- improving_hashes/spec.rb
+++ improving_hashes/spec.rb
@@ -33,21 +33,3 @@ describe Hash do
   end
 
 end
-
-  # describe '#initialize' do 
-
-  #   subject(:my_hash) { Hash.new( { :a => 100 } ) }
-    
-  #   it 'should create an instance of the class Hash' do 
-  #     subject.should be_true
-  #   end
-
-  #   it 'should have attribute key' do 
-  #     expect(subject.key).should be_true
-  #   end
-
-  #   it 'should have attribute value' do 
-  #     expect(subject.value).should be_true
-  #   end
-
-  # end
