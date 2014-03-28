93e17fc9ca1548f7c5e6ccbb66c3e11caefbe358
Steven Motley <stevenmotley89@gmail.com>
2014-03-28 10:12:30 -0400

    refactor if/else statement with ternary evaluator

--- slope/src/slope.js
+++ slope/src/slope.js
@@ -2,11 +2,6 @@ function getSlope(point1, point2) {
   var yDiff = point1.pop() - point2.pop();
   var xDiff = point1.pop() - point2.pop();
   var slope = yDiff / xDiff;
-  if (xDiff == 0) {
-    return null;
-  } else if (yDiff == 0) {
-    return 0;
-  } else {
-    return slope;
-  }
+  return isNaN(slope) || slope == Infinity ? null : slope
 }
+
