bbe1ef9f640f8a19b2e67c28b1150993bb08f829
Steven Motley <stevenmotley89@gmail.com>
2014-03-29 09:54:10 -0400

    write more tests for #getMagicNumber function

--- numerology/spec/numerologySpec.js
+++ numerology/spec/numerologySpec.js
@@ -8,6 +8,7 @@ describe("The Birthday constructor", function() {
     expect(birthday).toBeDefined();
     expect(birthday).not.toBe(null);
     expect(birthday).toBeTruthy();
+    expect(Object.getPrototypeOf(birthday)).toEqual({});
   });
 
   it('has a date attribute', function () {
@@ -15,14 +16,35 @@ describe("The Birthday constructor", function() {
     expect(typeof(birthday.date)).toEqual('string');
     expect(birthday.date.length).toEqual(10);
   });
-
 });
 
 //#getMagicNumber prototype tests
 describe("getMagicNumber", function() {
-  
-  it("should never return a value greater than 10", function() {
-    expect(date.getMagicNumber()).toBeLessThan(11)
+
+  it("should return the numerology for a birthday", function() {
+    var birthday = new Birthday("04/02/2008");
+    expect(birthday.getMagicNumber).toEqual(7)
+  });
+
+  it("should return the numerology for a birthday", function() {
+    var birthday = new Birthday("01/01/1998");
+    expect(birthday.getMagicNumber).toEqual(2)
+  });
+
+  it("should return the numerology for a birthday", function() {
+    var birthday = new Birthday("10/22/1977");
+    expect(birthday.getMagicNumber).toEqual(3)
+  });
+
+  it("should never return a value greater than 9", function() {
+
+    var birthday = new Birthday("10/22/1977");
+    expect(birthday.getMagicNumber).toBeLessThan(3)
   });
 
 });
+
+
+// In numerology, every number has a certain meaning that expresses someones connection to the universe! This single digit integer can be calculated by adding up every digit in the birthdate: year, month, and date. Each time the sum exceeds 9, add up the 2 digits.
+// For example, new Date('06/14/2010') = 5
+// So, what is your number?
--- numerology/src/numerology.js
+++ numerology/src/numerology.js
@@ -3,6 +3,9 @@ function Birthday(date) {
   this.date = date
 }
 
+Birthday.protoype.getMagicNumber = function() {
+  
+}
 
 // Line.prototype.getSlope = function() {
 //   var yDiff = this.point1.pop() - this.point2.pop();
