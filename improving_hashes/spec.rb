160dbe483f3b098b6dd9f46cde79b53a5d55003a
Steven Motley <stevenmotley89@gmail.com>
2014-03-30 09:07:55 -0400

    rename subject to 'my_hash'

--- improving_hashes/spec.rb
+++ improving_hashes/spec.rb
@@ -7,21 +7,21 @@ describe Hash do
     subject(:my_hash) { Hash['a', 100, 'b', 200] }
 
     it 'should return the value of the hash key' do 
-      expect(subject.a).to eq(100)
+      expect(my_hash.a).to eq(100)
     end
 
     it 'should return the value of the hash key' do 
-      expect(subject.b).to eq(200)
+      expect(my_hash.b).to eq(200)
     end
 
     it 'should return a NoMethodError exception' do 
-      expect(subject.c).to eq(NoMethodError)
+      expect(my_hash.c).to eq(NoMethodError)
     end
 
     subject(:my_hash) { {'a' => 1} }
     
     it 'should return the value of the hash key' do
-      expect(subject.a).to eq(1)
+      expect(my_hash.a).to eq(1)
     end
 
     context "Methods that already exist in Hash class" do 
@@ -29,15 +29,15 @@ describe Hash do
       subject(:my_hash) { Hash['a', 100, 'b', 200, 'c', 300] }
 
       it 'should respond properly to methods that already exist' do
-        expect(subject.size).to eq(2)
+        expect(my_hash.size).to eq(2)
       end
 
       it 'should respond propertly to methods that already exist' do 
-        expect(subject.values).to eq([100, 200, 300])
+        expect(my_hash.values).to eq([100, 200, 300])
       end
 
       it 'should respond properly to methods that already exist' do 
-        expect(subject.flatten).to eq([a, 100, b, 200, c, 300])
+        expect(my_hash.flatten).to eq([a, 100, b, 200, c, 300])
       end
 
     end
