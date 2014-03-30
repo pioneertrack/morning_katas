a384d804f063fd3911cf062ac4983cd424d98e10
Steven Motley <stevenmotley89@gmail.com>
2014-03-30 09:03:06 -0400

    add rspec context 'methods that already exist in Hash class' and write newtests

--- improving_hashes/spec.rb
+++ improving_hashes/spec.rb
@@ -10,8 +10,6 @@ describe Hash do
       expect(subject.a).to eq(100)
     end
 
-    subject(:my_hash) { Hash[:a, 100, :b, 200] }
-
     it 'should return the value of the hash key' do 
       expect(subject.b).to eq(200)
     end
@@ -20,15 +18,26 @@ describe Hash do
       expect(subject.c).to eq(NoMethodError)
     end
 
-    it 'should return the size of the hash if key is size' do
-      expect(subject.size).to eq(2)
-    end
-
     subject(:my_hash) { {'a' => 1} }
     
-    it 'should return the size of the hash if key is size' do
+    it 'should return the value of the hash key' do
       expect(subject.a).to eq(1)
     end
+
+    context "Methods that already exist in Hash class" do 
+
+      subject(:my_hash) { Hash['a', 100, 'b', 200, 'c', 300] }
+
+      it 'should respond properly to methods that already exist' do
+        expect(subject.size).to eq(2)
+      end
+
+      it 'should respond propertly to methods that already exist' do 
+        expect(subject.values).to eq([100, 200, 300])
+      end
+
+    end
+
 
   end
 
