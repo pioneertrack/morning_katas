d0c67292ce73cdc4821525cbbdae448785461c8f
Steven Motley <stevenmotley89@gmail.com>
2014-03-31 09:37:14 -0400

    write tests for initialize method

---
+++ trailing_zeros/spec.rb
@@ -0,0 +1,38 @@
+require './trailing_zeros.rb'
+
+describe '#intialize' do 
+
+  subject(:parser) = Parser.new(12)
+
+  it 'should create a new instance of the class Parser' do 
+    parser.is_a(Parser).should be_true
+  end
+
+  it 'should have an attribute named num' do 
+    parser.num.should be_true
+  end
+
+  it 'should respond to proper methods' do 
+    parser.should respond_to(:num)
+  end
+
+end
+
+describe '#zeros' do 
+
+end
+
+# Description:
+
+# Write a program that will calculate the number of trailing zeros in a factorial of a given number.
+
+# N! = 1 * 2 * 3 * 4 ... N
+
+# zeros(12) = 2 # 1 * 2 * 3 .. 12 = 479001600 
+# that has 2 trailing zeros 4790016(00)
+
+# Be careful 1000! has length of 2568 digital numbers.
+
+# def zeros(n)
+  
+# end
---
+++ trailing_zeros/trailing_zeros.rb
@@ -0,0 +1,14 @@
+# Description:
+
+# Write a program that will calculate the number of trailing zeros in a factorial of a given number.
+
+# N! = 1 * 2 * 3 * 4 ... N
+
+# zeros(12) = 2 # 1 * 2 * 3 .. 12 = 479001600 
+# that has 2 trailing zeros 4790016(00)
+
+# Be careful 1000! has length of 2568 digital numbers.
+
+# def zeros(n)
+  
+# end
