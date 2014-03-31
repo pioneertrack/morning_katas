68c55127d99755b1ab0af3a7870f2546c8228c3d
Steven Motley <stevenmotley89@gmail.com>
2014-03-31 16:44:02 -0400

    write method to return the max exponent of 5 that is less than self.num

--- trailing_zeros/spec.rb
+++ trailing_zeros/spec.rb
@@ -29,48 +29,33 @@ end
 describe '#zeros' do 
 
   it 'should calculate the number of trailing zeros of N!' do
-    parser = Parser.new(5)
-    expect(parser.zeros).to eq(1)
+    parser = Parser.new(32)
+    expect(parser.calculate_trailing_zeros).to eq(7)
   end
 
   it 'should calculate the number of trailing zeros of N!' do
     parser = Parser.new(10)
-    expect(parser.zeros).to eq(2)
+    expect(parser.calculate_trailing_zeros).to eq(2)
   end
 
   it 'should calculate the number of trailing zeros of N!' do
     parser = Parser.new(15)
-    expect(parser.zeros).to eq(3)
+    expect(parser.calculate_trailing_zeros).to eq(3)
   end
 
   it 'should calculate the number of trailing zeros of N!' do
     parser = Parser.new(20)
-    expect(parser.zeros).to eq(4)
+    expect(parser.calculate_trailing_zeros).to eq(4)
   end
 
   it 'should calculate the number of trailing zeros of N!' do
     parser = Parser.new(30)
-    expect(parser.zeros).to eq(6)
+    expect(parser.calculate_trailing_zeros).to eq(7)
   end
 
   it 'should calculate the number of trailing zeros of N!' do
     parser = Parser.new(50)
-    expect(parser.zeros).to eq(10)
+    expect(parser.calculate_trailing_zeros).to eq(12)
   end
 
-end
-
-# Description:
-
-# Write a program that will calculate the number of trailing zeros in a factorial of a given number.
-
-# N! = 1 * 2 * 3 * 4 ... N
-
-# zeros(12) = 2 # 1 * 2 * 3 .. 12 = 479001600 
-# that has 2 trailing zeros 4790016(00)
-
-# Be careful 1000! has length of 2568 digital numbers.
-
-# def zeros(n)
-  
-# end
+end 
--- trailing_zeros/trailing_zeros.rb
+++ trailing_zeros/trailing_zeros.rb
@@ -10,6 +10,7 @@ class Parser
 
   def factorial
     num = self.num
+    return 1 if num == 0
     current = num
     (num-1).times do
       current *= num-=1
@@ -17,24 +18,40 @@ class Parser
     current
   end
 
+  def find_max_k
+    current = 5
+    i = 1
+    while current < self.num
+      current**=i
+      i+=1
+    end
+    i-2
+  end
+
+  def calculate_trailing_zeros
+    num = self.num
+    current = 0
+    i = find_max_k
+    return 0 if num == 0
+    return num / 5 if find_max_k < 2
+    find_max_k.times do
+      current += num / 5**(i)
+      i-=1
+    end
+    current
+  end
+
+
   def count_trailing_zeros
     self.factorial.to_s.match(/(0*)$/)
-    $1.length
+    $1.nil? ? 0 : $1.length 
   end
 
 end
 
-# Description:
 
-# Write a program that will calculate the number of trailing zeros in a factorial of a given number.
-
-# N! = 1 * 2 * 3 * 4 ... N
-
-# zeros(12) = 2 # 1 * 2 * 3 .. 12 = 479001600 
-# that has 2 trailing zeros 4790016(00)
-
-# Be careful 1000! has length of 2568 digital numbers.
-
-# def zeros(n)
-  
-# end
+# Test Passed: Value == 0
+# Test Failed: Expected: 1, instead got: 0
+# Test Passed: Value == 7
+# Test Passed: Value == 24
+# Test Passed: Value == 249
