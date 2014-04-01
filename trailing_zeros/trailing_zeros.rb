b0083b4747bd6a0c76ecb90762b4b45b6c69a340
Steven Motley <stevenmotley89@gmail.com>
2014-04-01 07:52:39 -0400

    review code and refactor notes

--- trailing_zeros/trailing_zeros.rb
+++ trailing_zeros/trailing_zeros.rb
@@ -8,17 +8,17 @@ class Parser
     @num = num
   end
 
-  def factorial
+  def factorial #calculates self.num!
     num = self.num
-    return 1 if num == 0
     current = num
+    return 1 if num == 0
     (num-1).times do
       current *= num-=1
     end
     current
   end
 
-  def find_max_exponent #returns the max exponential of 5 that is less than self.num
+  def max_exponent #returns the max exponential of 5 that is less than self.num
     current = 0
     exponent = 0
     while current < self.num do 
@@ -27,19 +27,19 @@ class Parser
     exponent-1
   end
 
-  def calculate_trailing_zeros
+  def calculate_trailing_zeros #calculates N!'s trailing zeros via multiples of 5
     num = self.num
     current = 0
-    i = find_max_exponent
-    return 0 if num == 0 #handles edge case where 0! = 1
-    find_max_exponent.times do
-      current += num / 5**(i) #calculates the multiplicity of the prime factor 5 in n!
+    i = max_exponent
+    return 0 if num == 0 #returns 0 for edge case where 0! = 1, i.e., no trailing zeros
+    max_exponent.times do
+      current += num / 5**(i) #calculates the the trailing zeros via dividing num by multiples of 5
       i-=1
     end
     current
   end
 
-  def count_trailing_zeros
+  def count_trailing_zeros #alternate solution calculates N!'s trailing zero by literally counting them'
     self.factorial.to_s.match(/(0*)$/)
     $1.nil? ? 0 : $1.length 
   end
