e6130949782675e8097f7a7878d23b3c7db29925
Steven Motley <stevenmotley89@gmail.com>
2014-04-19 08:52:00 -0400

    make dictionary constructor with content and first attrs and make constructor tests pass

--- valid_strings/spec/validwordSpec.js
+++ valid_strings/spec/validwordSpec.js
@@ -1,17 +1,22 @@
 // Dictionary constructor tests
-describe("The Dictionary contructor", function() {
-  var dictionary = new Dictionary(evil, dead, rabbit, zombies);
+describe("The Dictionary constructor", function() {
+
+  var dictionary; 
+
+  beforeEach(function() {
+    dictionary = new Dictionary(['evil', 'dead', 'rabbit', 'zombies']);
+  });
 
   it('creates a new Dictionary object', function() {
     expect(dictionary).toBeDefined();
     expect(dictionary).not.toBe(null);
   });
 
-  it('returns an array of words', function() {
-    expect(dictionary).toContain('dead');
-    expect(dictionary).not.toContain('Dolly Parton');
-    expect(dictionary.length).toEqual(4);
-    expect(birthday.pop).toBeDefined();
+  it('should respond to proper methods', function() {
+    expect(dictionary.content).toContain('dead');
+    expect(dictionary.content).not.toContain('Dolly Parton');
+    expect(dictionary.content.length).toEqual(4);
+    expect(dictionary.content.pop).toBeDefined();
   });
 
   it('should respond to proper methods', function() {
@@ -25,12 +30,10 @@ describe("The Dictionary contructor", function() {
 describe("#validWord", function() {
 
   it("should return true if a given a string is comprised of one or more words form the dictionary", function() {
-    var dictionary = new Dictionary(evil, dead, rabbit, zombies);
     expect(dictionary.validWord('evildead')).toBe(true);
   });
 
   it("should return false if a string is not comprised of a word from the dictionary", function () {
-    var dictionary = new Dictionary(evil, dead, rabbit, zombies);
     expect(dictionary.validWord('Dolly Parton')).toBe(false);
   });
 });
--- valid_strings/src/validword.js
+++ valid_strings/src/validword.js
@@ -1,3 +1,8 @@
+function Dictionary(word_array) {
+  this.content = word_array 
+  this.first = this.content[0];
+}
+
 var validWord = function(dictionary, word) {
 
 };
