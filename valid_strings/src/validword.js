d5c6cf303cb1e134787dcede6c4f35a0f2d7fb06
Steven Motley <stevenmotley89@gmail.com>
2014-04-20 09:54:10 -0400

    add new counter variable j that increments only when a word index matches a string index; add extra conditional to while loop that requires word[j] exists

--- valid_strings/src/validword.js
+++ valid_strings/src/validword.js
@@ -18,21 +18,36 @@
   Dictionary.prototype.validWord = function (string) {
     var content = this.content;
     var output = [];
+    var sumLengthOfMatches = 0;
     //content = ["evil", "dead", "rabbit", "zombies"] 
     content.forEach(function (word) {
       var i = 0;
+      var j = 0;
       var results = [];
-      while (i < word.length) {
-        if (word[i] === string[i]) {
+      while (i < string.length && word[j] != undefined) {
+        //codewars
+        debugger
+        if (word[j] === string[i]) {
+          // cod codewars
           results.push(true);
+          j++;
         } else {
           results.push(false);
         }
-        i += 1;
+        i+=1;
       }
       outcome = results.every(allElementsAreTrue);
-      output.push(outcome);
-    });    
+      output.push(outcome);  
+    });   
     return oneElementIsTrue(output);
   };
+
+
+ // if the sum of the length of all winning matches != string.length ? false : output
+
+  // arghhhhhhhh
+  // dictionary = new Dictionary(['cod', 'dewa', 'ars']);
+  //   expect(dictionary.validWord('codewars')).toBe(false);
+//   > string = ['cod', 'dewa', 'ars'].join('');
+// 'coddewaars'
 
