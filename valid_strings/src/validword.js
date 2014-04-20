c5a945b639d735cf7315c8da66e0bcfa03a19f9b
Steven Motley <stevenmotley89@gmail.com>
2014-04-20 13:41:15 -0400

    add helper functions sortByLength and filterOutWordsThatContainOtherWords

--- valid_strings/src/validword.js
+++ valid_strings/src/validword.js
@@ -3,6 +3,36 @@
     this.first = this.content[0];
   }
 
+  var allElementsAreTrue = function(value, index, array) {
+    if (value === true) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+//   var a = 'Magic belongs to Jerry Harry Jerry Harry Potter and Banana Joe'.split(' ');
+// a = a.sort()
+// >>>["Banana", "Harry", "Harry", "Jerry", "Jerry", "Joe", "Magic", "Potter", "and", "belongs", "to"]
+// a.filter( function(v,i,o){if(i>=0 && v!==o[i-1]) return v;});
+
+  var sortByLength = function (array) {
+    array.sort(function(a,b){
+      return a.length - b.length;
+    });
+    return array;
+  }
+
+  var filterOutWordsThatContainOtherWords = function (array) {
+    var output;
+    sortByLength(array).filter(function (value, index, array) {
+      if (array[index+1].indexOf(value) !== -1) {
+        array.splice([index+1]);
+      }
+    });
+    return array
+  };
+
   var enoughElementsAreTrue = function (array, length) {
     //[false, false, true, true, true, true]
     i = 0;
@@ -13,7 +43,6 @@
     });
     return i == length ? true : false;
   };
-
   var oneElementIsTrue = function(array) {
     return array.indexOf(true) != -1 ? true : false;
   }
@@ -22,7 +51,8 @@
     var content = this.content;
     var matchArrayForEachWord = [];
     var sumLengthOfMatches = 0;
-    //content = ["evil", "dead", "rabbit", "zombies"] 
+    //Test.expect(validWord(['foob', 'bar', 'foo'], 'foobar') === true);
+    wordArray = filterOutWordsThatContainOtherWords(content);
     content.forEach(function (word) {
       var i = 0;
       var j = 0;
@@ -43,6 +73,10 @@
         matchArrayForEachWord.push(true);
       }   
     }); 
-    return sumLengthOfMatches > string.length ? false : oneElementIsTrue(matchArrayForEachWord);
+    if (matchArrayForEachWord.every(allElementsAreTrue)) {
+      return true;
+    } else {
+      return sumLengthOfMatches < string.length ? oneElementIsTrue(matchArrayForEachWord) : false;
+    }
   };
 
