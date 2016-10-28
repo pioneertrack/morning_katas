0f67132e751b4c2ebe590f4113417cd3e1f33440
Steven Motley <stevenmotley89@gmail.com>
2016-10-28 13:45:04 -0400

    refactor with es6 class syntax

--- array_calculator/src/Calculator.js
+++ array_calculator/src/Calculator.js
@@ -1,36 +1,38 @@
-function Calculator() {
-}
+class Calculator {
 
-// example use of recursion to handle nested arrays
-Calculator.prototype.sumNumbers = function(arr) {
-  if (Array.isArray(arr) == false) {
-    throw new Error("This function only accepts array arguments");
-  }
-  var sum = 0;
-  for (var i = 0; i < arr.length; i++) {
-    var type = typeof(arr[i]);
-    if (type == "number") {
-      sum += arr[i];
-    } else if (type == "object") {
-      sum += this.sumNumbers(arr[i]);
-    } else {
-      sum += 0;
-    }
-  }
-  return sum;
-}
 
-Calculator.prototype.uniqueValues = function(arr) {
-  // iterate over arr & uniques array
-  // if uniques doesn't include current arr value, push in array
-  var uniquesSoFar = []
-  for(var i=0; i<arr.length; i++) {
-    var exists = false; // set flag to false for each el in arr
-    for(var j=0; j<uniquesSoFar.length; j++) {
-      if (uniquesSoFar[j] == arr[i]) { exists = true; }
+  // example use of recursion to handle nested arrays
+  sumNumbers(arr) {
+    if (Array.isArray(arr) == false) {
+      throw new Error("This function only accepts array arguments");
     }
-    if (exists == false) { uniquesSoFar.push(arr[i]); }
+    var sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+      var type = typeof(arr[i]);
+      if (type == "number") {
+        sum += arr[i];
+      } else if (type == "object") {
+        sum += this.sumNumbers(arr[i]);
+      } else {
+        sum += 0;
+      }
+    }
+    return sum;
   }
-  return uniquesSoFar
+
+  uniqueValues(arr) {
+    // iterate over arr & uniques array
+    // if uniques doesn't include current arr value, push in array
+    var uniquesSoFar = []
+    for(var i=0; i<arr.length; i++) {
+      var exists = false; // set flag to false for each el in arr
+      for(var j=0; j<uniquesSoFar.length; j++) {
+        if (uniquesSoFar[j] == arr[i]) { exists = true; }
+      }
+      if (exists == false) { uniquesSoFar.push(arr[i]); }
+    }
+    return uniquesSoFar
+  }
+
 }
 
