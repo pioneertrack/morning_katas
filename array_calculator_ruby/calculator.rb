0a468c313e1baf6028902ab7ae7abc1ea720ec54
Steven Motley <stevenmotley89@gmail.com>
2016-11-02 11:19:18 -0400

    add methods to get unique vals from array and to calc max profit from sale of stock yesterday - text with rspec

--- array_calculator_ruby/calculator.rb
+++ array_calculator_ruby/calculator.rb
@@ -5,11 +5,11 @@ class Calculator
 		@optional_array = data
 	end
 
-	# get the sum of numbers for a multi-dimensional array
-	def sum_numbers(input_array=[])
-		raise ArgumentError, 'Argument is not an array' unless input_array.class == Array
+	# output the sum of numbers for a multi-dimensional array
+	def sum_numbers(arr=[])
+		check_argument(arr)
 		sum = 0
-		input_array.each do |val|
+		arr.each do |val|
 			if val.is_a? Fixnum
 				sum += val
 			elsif val.is_a? Array
@@ -19,5 +19,44 @@ class Calculator
 			end
 		end
 		return sum
+	end
+
+	# output the unique values in an array
+	def uniques(arr=[])
+		check_argument(arr)
+		uniques = []
+		arr.each do |val|
+			exists = false
+			uniques.each do |unique|
+				exists = true if unique == val
+			end
+			uniques << val if exists == false
+		end
+
+		return uniques
+	end
+
+	# get max profit from 1 purchase and 1 sale of 1 stock yesterday
+	def get_max_profit(stock_prices_yesterday=[])
+		max_profit = stock_prices_yesterday[1] - stock_prices_yesterday[0]
+		min_price = stock_prices_yesterday[0]
+
+		stock_prices_yesterday.each_with_index do |current_price, index|
+			# must skip time 0 bc we can't buy and sell at same price
+			next if index == 0
+
+			potential_profit = current_price - min_price
+
+			max_profit = [potential_profit, max_profit].max
+
+			min_price = [current_price, min_price].min
+		end
+		return max_profit
+	end
+
+	private
+
+	def check_argument(input)
+		raise ArgumentError, 'Argument is not an array' unless input.is_a? Array
 	end
 end
--- array_calculator_ruby/spec.rb
+++ array_calculator_ruby/spec.rb
@@ -18,12 +18,47 @@ describe Calculator do
 		subject(:calculator) { Calculator.new }
 
 		it 'should sum all the numbes in a multi-dimensional array' do
-			data = [1, 3, 'g', [2, 4, ['g', 2, 4]], 2]
-			expect(calculator.sum_numbers(data)).to eq(18)
+			input = [1, 3, 'g', [2, 4, ['g', 2, 4]], 2]
+			expect(calculator.sum_numbers(input)).to eq(18)
 		end
 
 		it 'should raise an exception if an argument other than an array is passed in' do
 			expect { calculator.sum_numbers('string') }.to raise_error(ArgumentError, 'Argument is not an array')
+		end
+	end
+
+	describe '#uniqes' do
+
+		subject(:calculator) { Calculator.new }
+		
+		it 'should get the unique values in an array' do
+			input = ['rabbit', 'dog', 'cat', 'rabbit', 'rabbit', 'dog', 'cat']
+			expect(calculator.uniques(input)).to eq(['rabbit', 'dog', 'cat'])
+		end
+
+		it 'should get the unique values in an array' do
+			input = [0, 2, 2, 4, 5, 4, 5, 0]
+			expect(calculator.uniques(input)).to eq([0, 2, 4, 5])
+		end
+
+		it 'should raise an exception if an argument other than an array is passed in' do
+			expect { calculator.uniques('string') }.to raise_error(ArgumentError, 'Argument is not an array')
+		end
+	end
+
+	describe '#get_max_profit' do
+
+		subject(:calculator) { Calculator.new }
+
+		it 'should return the max profit from 1 purchase and 1 sale of 1 stock yesterday' do
+			input = [10, 4, 3, 20]
+			expect(calculator.get_max_profit(input)).to eq(17)
+		end
+
+		# edge case when price goes all the way down
+		it 'should return the max profit from 1 purchase and 1 sale of 1 stock yesterday' do
+			input = [10, 8, 6, 3, 1]
+			expect(calculator.get_max_profit(input)).to eq(-2)
 		end
 	end
 end
