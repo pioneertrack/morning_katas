3dcc5f6874c64a0088dacd695cc4b725a3180480
Steven Motley <stevenmotley89@gmail.com>
2016-11-03 11:44:55 -0400

    add jasmine matchers

--- shuffle_cards_js/spec/DeckSpec.js
+++ shuffle_cards_js/spec/DeckSpec.js
@@ -13,9 +13,7 @@ describe("Deck", function() {
 
 	describe("#getRandomInclusive", function() {
 		it("should return a random number between the argument range", function() {
-			result = deck.getRandomIntInclusive(4, 6);
-			expect(result).toBeGreaterThan(3);
-			expect(result).not.toBeGreaterThan(6);
+			expect(deck.getRandomIntInclusive(4, 6)).toBeWithinRange(4,6);
 		});
 	});
 
