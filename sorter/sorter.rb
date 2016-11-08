4c2241f05fb61c985a00d79c1b6d8cac36453c35
Steven Motley <stevenmotley89@gmail.com>
2016-11-08 10:34:07 -0500

    less work to only save min and swap in outer loop

--- sorter/sorter.rb
+++ sorter/sorter.rb
@@ -9,16 +9,17 @@ class Sorter
 
 
 	def selection_sort(int_array=[])
+    #[3,2,1]
+    puts "INT ARRAY #{int_array}"
     for outer_index in (0...int_array.length) #3
       min_index = outer_index
       for inner_index in (outer_index+1...int_array.length)# 2
+        puts "if #{int_array[inner_index]} is less than #{int_array[min_index]} new min = #{inner_index}"
         if int_array[inner_index] < int_array[min_index]
-          # temp = int_array[inner_index]
-          # int_array[inner_index] = int_array[min_index]
-          # int_array[min_index] = temp
-          int_array[inner_index], int_array[min_index] = int_array[min_index], int_array[inner_index]
+          min_index = inner_index
         end
       end
+      int_array[outer_index], int_array[min_index] = int_array[min_index], int_array[outer_index]
     end
     int_array
 	end
