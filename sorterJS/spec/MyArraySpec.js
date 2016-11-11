bb3ec70745ce5d63729beb4050c6d1175cfe7310
Steven Motley <stevenmotley89@gmail.com>
2016-11-11 11:25:19 -0500

    solve quick sort in JS

--- sorterJS/spec/MyArraySpec.js
+++ sorterJS/spec/MyArraySpec.js
@@ -8,7 +8,13 @@ describe("MyArray", function() {
   describe("#selectionSort", function() {
     it("should sort all the numbers in an array", function() {
       const int_array = [10, 0, -1, 5, 4, 8, 20];
-      debugger
+      expect(myArray.selectionSort(int_array)).toEqual([-1, 0, 4, 5, 8, 10, 20]);
+    });
+  });
+
+  describe("#quickSort", function() {
+    it("should sort all the numbers in an array", function() {
+      const int_array = [10, 0, -1, 5, 4, 8, 20];
       expect(myArray.selectionSort(int_array)).toEqual([-1, 0, 4, 5, 8, 10, 20]);
     });
   });
--- sorterJS/src/MyArray.js
+++ sorterJS/src/MyArray.js
@@ -1,23 +1,41 @@
 class MyArray {
 
- constructor(arr) {
-   this.array = arr;
- }
+	constructor(arr) {
+		this.array = arr;
+	}
 
- selectionSort(int_array) {
- 	for(var i=0; i < int_array.length; i++) {
- 		let min_index = i;
- 		for(var j=i+1; j < int_array.length; j++) {
- 			if (int_array[j] < int_array[min_index]) {
- 				min_index = j;
- 			}
- 		}
- 		// swap 'em
- 		let temp = int_array[i];
- 		int_array[i] = int_array[min_index];
- 		int_array[min_index] = temp;
- 	}
- 	return int_array;
- }
+	selectionSort(int_array) {
+		for(var i=0; i < int_array.length; i++) {
+			let min_index = i;
+			for(var j=i+1; j < int_array.length; j++) {
+				if (int_array[j] < int_array[min_index]) {
+					min_index = j;
+				}
+			}
+			// swap 'em
+			let temp = int_array[i];
+			int_array[i] = int_array[min_index];
+			int_array[min_index] = temp;
+		}
+		return int_array;
+	}
+
+	quickSort(int_array) {
+		if (int_array.length == 0) return [];
+		let rand = Math.random(int_array.length);
+		let pivot = int_array.splice(rand, 1); // returns element in array from array at rand index
+
+		//partition
+		let left = [];
+		let right = [];
+		for (var i=0; i<int_array.lenth; i++) {
+			if (int_array[i] < int_array[pivot]) {
+				left.push(int_array[i]);       
+			} else {
+				right.push(int_array[i]);
+			}
+		}
+		this.quickSort(left).concat(pivot, this.quickSort(right));
+	}   
 
 }
