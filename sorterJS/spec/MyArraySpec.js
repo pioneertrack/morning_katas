49a92bd644d07dca4191f9af561053c3c8ddd28b
Steven Motley <stevenmotley89@gmail.com>
2016-11-12 10:10:44 -0500

    add trueQuickSort solution where we swap array elements in place

--- sorterJS/spec/MyArraySpec.js
+++ sorterJS/spec/MyArraySpec.js
@@ -18,4 +18,11 @@ describe("MyArray", function() {
       expect(myArray.selectionSort(int_array)).toEqual([-1, 0, 4, 5, 8, 10, 20]);
     });
   });
+
+  describe("#trueQuickSort", function() {
+    it("should sort all the numbers in an array", function() {
+      const int_array = [10, 0, -1, 5, 4, 8, 20];
+      expect(myArray.selectionSort(int_array)).toEqual([-1, 0, 4, 5, 8, 10, 20]);
+    });
+  });
 });
--- sorterJS/src/MyArray.js
+++ sorterJS/src/MyArray.js
@@ -36,6 +36,39 @@ class MyArray {
 			}
 		}
 		this.quickSort(left).concat(pivot, this.quickSort(right));
-	}   
+	}
 
+	trueQuickSort(int_array, left, right){
+		console.log("ARRAY" + int_array + "," + "LEFT" + left + "," + "RIGHT" + right);
+		let partitionIndex;
+		let rand = Math.random(int_array.length);
+		let pivot = int_array[rand];
+
+		console.log("PIVOT" + pivot);
+
+
+		if (left < right) { // base case, if left is not less than right we've nothing left to partition
+      partitionIndex = left;
+			for (var i = left; i < right; i++) {
+				// swap els in array if less than pivot
+				if (int_array[i] < pivot) {
+					this.swap(int_array, i, partitionIndex);
+					partitionIndex ++ // increment pivot only if we swap
+				}
+			}
+
+			this.swap(int_array, rand, partitionIndex) // last make sure we put pivot is in the right position
+			//sort left and right
+			this.trueQuickSort(int_array, left, partitionIndex - 1);
+			this.trueQuickSort(int_array, partitionIndex + 1, right);
+		}
+		return int_array;
+	}
+
+	swap(array, i, j) {
+		let temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}  
 }
+
