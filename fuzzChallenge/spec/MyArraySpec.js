dcb001d3c928c7e6e37436f4f469a76d17b7f45b
Steven Motley <stevenmotley89@gmail.com>
2017-02-08 18:32:49 -0500

    solve by recursion

--- fuzzChallenge/spec/MyArraySpec.js
+++ fuzzChallenge/spec/MyArraySpec.js
@@ -22,4 +22,22 @@ describe("MyArray", function() {
       expect(array.incrementNumberArrayWhileLoop([9, 9, 9])).toEqual([1, 0, 0, 0]);
     });
   });
+
+  describe("#incrementNumberArryRecursion", function() {
+    it("should increment the number array by one, as if it were a number", function() {
+      expect(array.incrementNumberArrayRecursion([1, 2, 3], 2)).toEqual([1, 2, 4]);
+    });
+
+    it("should increment the number array by one, as if it were a number", function() {
+      expect(array.incrementNumberArrayRecursion([1, 2, 9], 2)).toEqual([1, 3, 0]);
+    });
+
+    it("should increment the number array by one, as if it were a number", function() {
+      expect(array.incrementNumberArrayRecursion([1, 9, 9], 2)).toEqual([2, 0, 0]);
+    });
+
+    it("should increment the number array by one, as if it were a number", function() {
+      expect(array.incrementNumberArrayRecursion([9, 9, 9], 2)).toEqual([1, 0, 0, 0]);
+    });
+  });
 })
--- fuzzChallenge/src/MyArray.js
+++ fuzzChallenge/src/MyArray.js
@@ -17,4 +17,20 @@ class MyArray {
 		}
 		return arr;
 	}
+
+	incrementNumberArrayRecursion(arr, i) {
+		if (i < 0) {return arr;}
+		if (arr[i] < 9) {
+			arr[i] += 1;
+			return arr;
+		} else if (i === 0) {
+			arr[i] = 0;
+			arr.unshift(1);
+			return arr;
+		} else {
+			arr[i] = 0;
+			this.incrementNumberArrayRecursion(arr, i-1);
+		}
+		return arr;
+	}
 }
